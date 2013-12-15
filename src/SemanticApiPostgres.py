b4e7d3f9937069f222ac3a5180662f3471778eb0
Uldis Bojars <captsolo@gmail.com>
2013-12-16 01:52:52 +0200

    refactoring Postgres API

--- src/SemanticApiPostgres.py
+++ src/SemanticApiPostgres.py
@@ -4,6 +4,8 @@
 import psycopg2, psycopg2.extensions
 import atexit
 
+from pprint import pprint
+
 # no FAQ - lai simbolus atgriež kā python unicode tipu
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
@@ -13,7 +15,7 @@ default_conn_info = {
         "host":"alnis.ailab.lv", "port": 5555, "dbname":"semantica", "user":"martins", "password":"parole",
     }
 
-class SemanticDbApi(object):
+class PostgresConnection(object):
 
     def __init__(self, conn_info=None, dataset=None):
         # FIXME
@@ -33,23 +35,63 @@ class SemanticDbApi(object):
 
         self.dataset = dataset
 
+        # FIXME - XXX
+        #  - statistika jāpārtaisa - nestrādās kad API operācijas ir iznestas atsevišķā klasē
         self.cSearchByName = 0  # counteri DB operācijām
         self.cInsertEntity = 0  
         self.cInsertFrame = 0  
 
         atexit.register(self.finalize)
 
+    # Statistika par DB requestiem to profilēšanai
+    def finalize(self):
+        print 'Kopā Postgres DB darbības:'
+        print self.cSearchByName, 'searchByName'
+        print self.cInsertEntity, 'insertEntity'
+        print self.cInsertFrame, 'insertFrame'
+        self.conn.commit()
+        self.conn.close()
 
-    # Saņem vārdu, atgriež sarakstu ar ID kas tiem vārdiem atbilst
-    def searchByName(self, name):
+    def commit(self):
+        self.conn.commit()
+
+    def query(self, sql, parameters):
         cursor = self.conn.cursor()
-        cursor.execute("select entityid from entityothernames where name = %s", (name,) )
+        cursor.execute(sql, parameters)
         r = cursor.fetchall()
         cursor.close()
 
-        self.cSearchByName += 1
+        return r
 
-        return map(lambda x: x[0], r) # kursors iedod sarakstu ar tuplēm, mums vajag sarakstu ar tīriem elementiem
+    def insert(self, sql, parameters, returning=False, commit=False):
+        cursor = self.conn.cursor()
+
+        cursor.execute(sql, parameters)
+        if returning:
+            result = cursor.fetchone()[0] # izņemam insertotā objekta id no tuples
+        else:
+            result = None
+
+        if commit:
+            self.commit()
+        cursor.close()
+
+        #self.cInsertEntity += 1
+
+        return result
+
+
+class SemanticApiPostgres(object):
+    def __init__(self, api):
+        self.api = api
+
+    # Saņem vārdu, atgriež sarakstu ar ID kas tiem vārdiem atbilst
+    def searchByName(self, name):
+        sql = "select entityid from entityothernames where name = %s"
+        res = self.api.query(sql, (name,) )
+        #self.cSearchByName += 1
+
+        return map(lambda x: x[0], res) # kursors iedod sarakstu ar tuplēm, mums vajag sarakstu ar tīriem elementiem
 
 	# Inserto jaunu entītiju datubāzē
 	# name - unicode string
@@ -58,21 +100,24 @@ class SemanticDbApi(object):
 	# outerids - list of unicode strings
 	# inflections - unicode string
     def insertEntity(self, name, othernames, category, outerids=[], inflections = None):
-        cursor = self.conn.cursor()
+        main_sql = "INSERT INTO Entities(Name, OtherNames, OuterID, category, DataSet, NameInflections) VALUES (%s, %s, %s, %s, %s, %s) RETURNING EntityID;"
 
-        cursor.execute(
-            "INSERT INTO Entities(Name, OtherNames, OuterID, category, DataSet, NameInflections) VALUES (%s, %s, %s, %s, %s, %s) RETURNING EntityID;" ,
-            (name, bool(othernames), bool(outerids), category, dataset, inflections) )
-        entityid = cursor.fetchone()[0] # izņemam insertotās rindas id no tuples
+        res = self.api.insert(main_sql, (name, bool(othernames), bool(outerids), category, dataset, inflections),
+                returning = True,
+                commit = False)
+        entityid = res # insertotās rindas id
 
+        names_sql = "INSERT INTO EntityOtherNames(EntityID, Name) VALUES (%s, %s)"
         for othername in othernames:
-            cursor.execute("INSERT INTO EntityOtherNames(EntityID, Name) VALUES (%s, %s)", (entityid, othername) )
-        for outerid in outerids:
-            cursor.execute("INSERT INTO EntityOuterIDs(EntityID, OuterID) VALUES (%s, %s)", (entityid, outerid) )       
-        self.conn.commit()
-        cursor.close()
+            self.api.insert(names_sql, (entityid, othername) )
 
-        self.cInsertEntity += 1
+        outerid_sql = "INSERT INTO EntityOuterIDs(EntityID, OuterID) VALUES (%s, %s)"
+        for outerid in outerids:
+            self.api.insert(outerid_sql, (entityid, outerid) )       
+
+        self.api.commit()
+
+        #self.cInsertEntity += 1
 
         return entityid
 
@@ -84,30 +129,38 @@ class SemanticDbApi(object):
 	# targetword - unicode string
 	# date - freima datums - string ISO datumformātā 
     def insertFrame(self, frametype, elements, document, source=None, sentenceId=None, targetword = None, date=None):
-        cursor = self.conn.cursor()
+        main_sql = "INSERT INTO Frames(FrameTypeID, SourceID, SentenceID, DocumentID, TargetWord, ApprowedTypeID, DataSet, Blessed, Hidden, Fdatetime)\
+                         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING FrameID;"
 
-        cursor.execute( "INSERT INTO Frames(FrameTypeID, SourceID, SentenceID, DocumentID, TargetWord, ApprowedTypeID, DataSet, Blessed, Hidden, Fdatetime)\
-                         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING FrameID;" ,
-                        (frametype, source, sentenceId, document, targetword, 0, dataset, None, False, date) )
-        frameid = cursor.fetchone()[0] # izņemam insertotās freima id no tuples
+        res = self.api.insert(main_sql,
+                (frametype, source, sentenceId, document, targetword, 0, dataset, None, False, date),
+                returning = True,
+                commit = False)
+        frameid = res # insertotā freima id
 
+        element_sql = "INSERT INTO FrameData(FrameID, EntityID, RoleID) VALUES (%s, %s, %s)"
         for element, entityid in elements.iteritems():
-            cursor.execute("INSERT INTO FrameData(FrameID, EntityID, RoleID) VALUES (%s, %s, %s)", (frameid, entityid, element) )       
+            self.api.insert(element_sql, (frameid, entityid, element) )       
             # NB! Te nav validācijas; te arī neuzstāda wordindex lauku
 
-        self.conn.commit()
-        cursor.close()
+        self.api.commit()
 
-        self.cInsertFrame += 1    
+        #self.cInsertFrame += 1    
 
         return frameid
 
-    # Statistika par DB requestiem to profilēšanai
-    def finalize(self):
-        print 'Kopā Postgres DB darbības:'
-        print self.cSearchByName, 'searchByName'
-        print self.cInsertEntity, 'insertEntity'
-        print self.cInsertFrame, 'insertFrame'
-        self.conn.commit()
-        self.conn.close()
 
+# ------------------------------------------------------------  
+
+def test_entity_select():
+    conn = PostgresConnection(default_conn_info)
+    api = SemanticApiPostgres(conn)
+
+    id_list = api.searchByName("Imants Ziedonis")
+    pprint(id_list)
+
+def main():
+    test_entity_select()
+
+if __name__ == "__main__":
+    main()
