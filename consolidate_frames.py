2f916b60374561a16e2c66cefb051224310e77b3
Uldis Bojars <captsolo@gmail.com>
2013-12-24 01:02:29 +0200

    cleanup

--- consolidate_frames.py
+++ consolidate_frames.py
@@ -5,22 +5,17 @@ import sys
 sys.path.append("./src")
 
 from pprint import pprint
-from itertools import chain
 import logging as log
 import os.path
-import csv
-import cPickle as pickle
 from datetime import datetime
 import os
-
-import requests
 
 import EntityFrames as EF
 
 from db_config import api_conn_info
 from SemanticApiPostgres import SemanticApiPostgres, PostgresConnection
 
-from ConsolidateFrames import Consolidator, BaseConsolidator
+from ConsolidateFrames import BaseConsolidator
 from FrameInfo import FrameInfo
 
 from TextGenerator import get_mentioned_entities, get_frame_text
@@ -187,16 +182,10 @@ def save_entity_frames_to_api(api, entity_list):
                 log.debug("Error saving frame to the API: (%s, %s)\n", is_ok["Answer"], is_ok["AnswerTypeString"])
                 error_frames.append(frame)
 
-        #for frame in data:
-        #    if frame["FrameData"] is not None:
-        #        out.writerow(format_frame_for_output(frame))
         log.info("")
 
         log.info("Save_entity_frames_to_api - completed.")
-        # print "\nSave_entity_frames_to_api - completed."
-
         log.info(" - list of frame IDs (for %s frames saved):\n%s", len(summary_frame_ids), repr(summary_frame_ids))
-        # print " - list of frame IDs (for %s frames saved):\n\n%s" % (len(summary_frame_ids), repr(summary_frame_ids))
 
         if len(error_frames)>0:
             log.info(" - %s frames could not be saved, returned errors.", len(error_frames))
@@ -217,99 +206,6 @@ def save_entity_frames_to_api(api, entity_list):
         print "%s\t%s" % (entity.entity_id, status)
         sys.stdout.flush()
 
-def save_entity_frames(out_dir, entity_list):
-
-    for entity in entity_list:
-
-        fname = os.path.join(out_dir, str(entity.entity_id) + ".csv")
-        log.info("Saving frame data to file: %s", fname)
-
-        with open(fname, "wb") as outf:
-
-            out = csv.writer(outf)
-
-            out.writerow((entity.entity["Name"].encode("utf-8"), entity.entity["EntityId"]))
-            out.writerow([])
-
-            out.writerow(("Total frames:", len(entity.frames)))
-            out.writerow(("Consolidated frames:", len(entity.cons_frames)))
-            out.writerow([])
-
-            out.writerow(("> Consolidated frames: <",))
-            out.writerow([])
-
-            # output consolidated frames
-
-            out.writerow(format_header_for_output())
-            data = sorted(entity.cons_frames, key = lambda x: x["FrameType"])
-
-            for frame in data:
-                if frame["FrameData"] is not None:
-                    out.writerow(format_frame_for_output(frame))
-
-            out.writerow([])
-
-            out.writerow(("> Original frames: <",))
-            out.writerow([])
-
-            # output original frames
-
-            out.writerow(format_header_for_output())
-            data = sorted(entity.frames, key = lambda x: x["FrameType"])
-            for frame in data:
-                if frame["FrameData"] is not None:
-                    out.writerow(format_frame_for_output(frame))
-
-            out.writerow(("-"*80,))
-
-        # print "Frames for e_id = [%s] written to: %s" % (entity.entity_id, fname)
-
-def format_header_for_output():
-    return (
-        "Status",
-        "Type",
-        "Count",
-        "SourceId",
-#       // FrameId -> not using for now, need to taker FrameIdList into account
-    )
-
-def format_frame_for_output(frame):
-    buf = [
-        frame.get("FrameStatus", ""),
-
-        frame["FrameType"],
-        frame.get("FrameCnt", ""),
-        frame["SourceId"],
-#       // FrameId -> not using for now, need to taker FrameIdList into account
-    ]
-
-    buf.extend(chain((item["Key"], item["Value"]["Entity"]) for item in frame["FrameData"])) 
-
-    return buf
-
-
-def print_entity_frames(entity_list):
-    for entity in entity_list:
-
-        log.info("-"*60)
-        log.info("Entity name: %s\tId: %s", entity.entity["Name"], entity.entity["EntityId"])
-
-        log.info("Frames (Total/Consolidated): %s / %s", len(entity.frames), len(entity.cons_frames))
-
-        for frame in entity.cons_frames:
-            frametext = frame.get("FrameText")
-            frame_type = frame["FrameType"]
-
-            # if not frametext is None:
-            #     print frametext.encode("utf8")
-            #     None
-            # else: 
-            #     print "None - ", f_info.type_name_from_id(frame_type).encode("utf8"), frame                
-
-#        print "> Original frames: <"
-#        print
-#        pprint(entity.frames)
-        
 
 def process_entities(entity_list, out_dir, api):
     data = list(get_entity_frames(entity_list, api))
@@ -317,46 +213,13 @@ def process_entities(entity_list, out_dir, api):
 
     frames = list(gen_frames)
 
-    print_entity_frames(frames)
-
-    if False:       # skip saving to pickle file (connection info can't be pickled) - FIXME if needed for debug reasons
-        """
-Traceback (most recent call last):
-  File "./demo_consolidate_frames.py", line 393, in <module>
-    main()
-  File "./demo_consolidate_frames.py", line 368, in main
-    process_entities(chunk, out_dir)
-  File "./demo_consolidate_frames.py", line 328, in process_entities
-    pickle.dump(frames, outf)   # consolidated frames
-  File "/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/copy_reg.py", line 70, in _reduce_ex
-    raise TypeError, "can't pickle %s objects" % base.__name__
-TypeError: can't pickle connection objects
-"""
-        #if len(entity_list) == 1:       
-        # in case of 1 entity in the list
-        # name the dump file according to the entity ID
-
-        fname = "output/%s.pickle" % (entity_list[0],)
-        log.debug("Dumping resulting frame list to: %s", fname)
-
-        with open(fname, "wb") as outf:
-            # frames = a list of [ ... EF.EntityFrames(api, e_id) ... ]
-            pickle.dump(frames, outf)   # consolidated frames
-
-            # reading the pickle file:
-
-            # import sys
-            # sys.path.append("src")
-            # import cPickle as pickle
-            # import EntityFrames as EF
-            # data = pickle.load(open(fname, "rb")) 
-
-            # e_info = data[0]    # entity that was consolidated
-            # [i for i in e_info.cons_frames if i["FrameId"] == 1264893]
-
-    #save_entity_frames(out_dir, frames)
+    for fr in frames:
+        log.info("-"*60)
+        log.info("Entity name: %s\tId: %s", fr.entity["Name"], fr.entity["EntityId"])
+        log.info("Frames (Total/Consolidated): %s / %s", len(fr.frames), len(fr.cons_frames))
 
     save_entity_frames_to_api(api, frames)
+
 
 def entity_ids_from_stdin():
     """
@@ -370,7 +233,7 @@ def entity_ids_from_stdin():
 
 def main():
     start_logging(log.DEBUG) #log.INFO)
-    log.info("Starting %s" % (sys.argv[0]))
+    log.info("Starting %s", sys.argv[0])
 
     out_dir = "./output"
     log.info("Output directory: %s\n", out_dir)
@@ -400,7 +263,6 @@ def start_logging(log_level = log.ERROR):
         format = "%(asctime)s: %(name)s: %(levelname)s: %(message)s",
     )
 
-    log.getLogger("requests.packages.urllib3").level = log.ERROR
     log.getLogger("SemanticApiPostgres").level = log.INFO
 
 # ---------------------------------------- 
