5db996683c725debe30b910a223eb6bf8e3fd6b7
Pēteris Paikens <PeterisP@gmail.com>
2015-04-15 14:34:08 +0300

    Extra relationship entities

--- src/Relationships.py
+++ src/Relationships.py
@@ -152,6 +152,13 @@ def isRelationshipName(name):
 		inflections = inflectEntity(name, 'relationship')
 		for inflection in json.loads(inflections).values():
 			__relationship_names.append(name)
+
+	for result in secondary_relations_text.values():
+		name = result.get('result')
+		__relationship_names.append(name)
+		inflections = inflectEntity(name, 'relationship')
+		for inflection in json.loads(inflections).values():
+			__relationship_names.append(name)
         
 	return name in __relationship_names
 
